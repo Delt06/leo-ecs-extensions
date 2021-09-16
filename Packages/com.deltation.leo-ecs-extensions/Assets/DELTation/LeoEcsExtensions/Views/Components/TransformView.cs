afe3668d23f9c5adb3c692562aef02c49ed1ec30
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-09-16 22:40:13 +0300

    Add transform view

---
+++ Packages/com.deltation.leo-ecs-extensions/Assets/DELTation/LeoEcsExtensions/Views/Components/TransformView.cs
@@ -0,0 +1,7 @@
+﻿using DELTation.LeoEcsExtensions.Components;
+using UnityEngine;
+
+namespace DELTation.LeoEcsExtensions.Views.Components
+{
+    public class TransformView : ComponentView<UnityObjectData<Transform>> { }
+}
