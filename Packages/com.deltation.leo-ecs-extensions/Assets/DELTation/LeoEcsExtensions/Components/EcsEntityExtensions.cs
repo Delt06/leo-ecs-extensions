41a259a5fd08e27ec1d7bd0799a5342e47709a45
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-09-18 18:03:50 +0300

    Add EcsEntityExtensions.OnUpdated

---
+++ Packages/com.deltation.leo-ecs-extensions/Assets/DELTation/LeoEcsExtensions/Components/EcsEntityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Leopotam.Ecs;
+
+namespace DELTation.LeoEcsExtensions.Components
+{
+    public static class EcsEntityExtensions
+    {
+        public static void OnUpdated<T>(this EcsEntity entity) where T : struct
+        {
+#if DEBUG
+            if (entity.IsNull()) throw new ArgumentNullException(nameof(entity));
+#endif
+            entity.Get<UpdateEvent<T>>();
+        }
+    }
+}
