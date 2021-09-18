9e7c58b7a5a8469b1d34cdc240755ad23afeff25
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-09-18 18:04:18 +0300

    Add preprocessor directives (DEBUG) to EcsEntityExtensions

--- Packages/com.deltation.leo-ecs-extensions/Assets/DELTation/LeoEcsExtensions/Views/EcsEntityExtensions.cs
+++ Packages/com.deltation.leo-ecs-extensions/Assets/DELTation/LeoEcsExtensions/Views/EcsEntityExtensions.cs
@@ -11,8 +11,10 @@ namespace DELTation.LeoEcsExtensions.Views
     {
         public static void SetViewBackRef<T>(this EcsEntity entity, [NotNull] T view) where T : IEntityView
         {
+#if DEBUG
             if (entity.IsNull()) throw new ArgumentNullException(nameof(entity));
             if (view == null) throw new ArgumentNullException(nameof(view));
+#endif
 
             ref var viewBackRef = ref entity.Get<ViewBackRef<T>>();
             viewBackRef.View = view;
@@ -20,8 +22,10 @@ namespace DELTation.LeoEcsExtensions.Views
 
         public static void SetUnityObjectData<T>(this EcsEntity entity, [NotNull] T @object) where T : Object
         {
+#if DEBUG
             if (entity.IsNull()) throw new ArgumentNullException(nameof(entity));
             if (@object == null) throw new ArgumentNullException(nameof(@object));
+#endif
 
             ref var unityObjectData = ref entity.Get<UnityObjectData<T>>();
             unityObjectData.Object = @object;
@@ -29,8 +33,10 @@ namespace DELTation.LeoEcsExtensions.Views
 
         public static void SetTransformComponentsFromTransform(this EcsEntity entity, [NotNull] Transform transform)
         {
+#if DEBUG
             if (entity.IsNull()) throw new ArgumentNullException(nameof(entity));
             if (transform == null) throw new ArgumentNullException(nameof(transform));
+#endif
 
             entity.Get<Position>().WorldPosition = transform.position;
             entity.Get<Rotation>().WorldRotation = transform.rotation;
