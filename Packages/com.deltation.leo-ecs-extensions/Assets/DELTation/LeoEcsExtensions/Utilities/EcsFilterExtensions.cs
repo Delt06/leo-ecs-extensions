f8999b57125977e0e916c093f495b2f19500153c
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-12-15 21:52:13 +0300

    Add IsEmpty for filters

---
+++ Packages/com.deltation.leo-ecs-extensions/Assets/DELTation/LeoEcsExtensions/Utilities/EcsFilterExtensions.cs
@@ -0,0 +1,29 @@
+﻿#if LEOECS_EXTENSIONS_LITE
+
+using System;
+using JetBrains.Annotations;
+using Leopotam.EcsLite;
+
+namespace DELTation.LeoEcsExtensions.Utilities
+{
+    public static class EcsFilterExtensions
+    {
+        public static bool IsEmpty([NotNull] this EcsFilter filter)
+        {
+#if DEBUG
+            if (filter == null) throw new ArgumentNullException(nameof(filter));
+#endif
+            return filter.GetEntitiesCount() == 0;
+        }
+
+        public static bool IsEmpty([NotNull] this EcsPackedFilter filter)
+        {
+#if DEBUG
+            if (filter == null) throw new ArgumentNullException(nameof(filter));
+#endif
+            return filter.GetEntitiesCount() == 0;
+        }
+    }
+}
+
+#endif
