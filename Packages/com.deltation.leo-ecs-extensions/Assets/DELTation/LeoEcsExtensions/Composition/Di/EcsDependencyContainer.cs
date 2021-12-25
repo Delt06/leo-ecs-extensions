7bc63136e2dce47078877544575ee96e34fdb665
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-12-25 18:30:39 +0300

    Rename WorldDependencyContainer to EcsDependencyContainer

--- Packages/com.deltation.leo-ecs-extensions/Assets/DELTation/LeoEcsExtensions/Composition/Di/EcsDependencyContainer.cs
+++ Packages/com.deltation.leo-ecs-extensions/Assets/DELTation/LeoEcsExtensions/Composition/Di/EcsDependencyContainer.cs
@@ -1,45 +1,47 @@
+#if LEOECS_EXTENSIONS_LITE
+
 using System;
 using System.Collections.Generic;
 using DELTation.DIFramework;
 using DELTation.LeoEcsExtensions.Services;
-using UnityEngine;
-#if LEOECS_EXTENSIONS_LITE
 using Leopotam.EcsLite;
-
-#else
-using Leopotam.Ecs;
-#endif
+using UnityEngine;
 
 namespace DELTation.LeoEcsExtensions.Composition.Di
 {
-    public class WorldDependencyContainer : MonoBehaviour, IDependencyContainer
+    public class EcsDependencyContainer : MonoBehaviour, IDependencyContainer
     {
         public bool TryResolve(Type type, out object dependency)
         {
             if (type == null) throw new ArgumentNullException(nameof(type));
-            if (!CanBeResolvedSafe(type))
+
+            if (DIFramework.Di.TryResolveGlobally(out IActiveEcsWorld activeEcsWorld))
             {
-                dependency = default;
-                return false;
+                var world = activeEcsWorld.World;
+
+                if (IsEcsWorld(type))
+                {
+                    dependency = world;
+                    return true;
+                }
             }
 
-            if (!DIFramework.Di.TryResolveGlobally(out IActiveEcsWorld activeEcsWorld))
-            {
-                dependency = default;
-                return false;
-            }
 
-            dependency = activeEcsWorld.World;
-            return true;
+            dependency = default;
+            return false;
         }
 
         public bool CanBeResolvedSafe(Type type)
         {
             if (type == null) throw new ArgumentNullException(nameof(type));
-            return typeof(EcsWorld).IsAssignableFrom(type) &&
+            return IsEcsWorld(type) &&
                    DIFramework.Di.CanBeResolvedGloballySafe<IActiveEcsWorld>();
         }
 
         public void GetAllRegisteredObjects(ICollection<object> objects) { }
+
+        private static bool IsEcsWorld(Type type) => typeof(EcsWorld).IsAssignableFrom(type);
     }
 }
+
+#endif
