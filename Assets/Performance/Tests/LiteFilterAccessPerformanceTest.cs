c3fa6c4fae19e7f46286b8e14373861da7b40fd8
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-01-14 21:57:01 +0300

    Add filter component access benchmark

---
+++ Assets/Performance/Tests/LiteFilterAccessPerformanceTest.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+using Leopotam.EcsLite;
+using Performance.Systems;
+
+namespace Performance.Tests
+{
+    [UsedImplicitly]
+    public class LiteFilterAccessPerformanceTest : FilterPerformanceTestBase
+    {
+        protected override IEcsSystem CreateSystem(EcsWorld world) => new LiteFilterAccessBenchmarkSystem(world);
+    }
+}
