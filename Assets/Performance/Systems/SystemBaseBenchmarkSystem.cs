d1407e8cd2d2c00aac913e49e83c4f984ffab723
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-02-28 00:26:28 +0300

    Refactor SystemBaseBenchmarkSystem

--- Assets/Performance/Systems/SystemBaseBenchmarkSystem.cs
+++ Assets/Performance/Systems/SystemBaseBenchmarkSystem.cs
@@ -21,17 +21,4 @@ namespace Performance.Systems
             }
         }
     }
-
-    public class SystemBase2BenchmarkSystem : EcsSystemBase2, IEcsRunSystem
-    {
-        public void Run(EcsSystems systems)
-        {
-            foreach (var i in World.Filter<PositionsComponent>().End())
-            {
-                ref var position = ref Get<PositionsComponent>(i);
-                var direction = Vector3.ClampMagnitude(position.P1 - position.P2, 1f);
-                GetOrAdd<DirectionComponent>(i).Direction = direction;
-            }
-        }
-    }
 }
