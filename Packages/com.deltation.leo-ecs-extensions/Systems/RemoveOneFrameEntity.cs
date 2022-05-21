ca2f6ff39d94b535fecca1dc05c73733ac48c179
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-05-21 11:11:04 +0300

    Add OneFrameEntity<T> [closes #12]

---
+++ Packages/com.deltation.leo-ecs-extensions/Systems/RemoveOneFrameEntity.cs
@@ -0,0 +1,30 @@
+﻿using Leopotam.EcsLite;
+
+namespace DELTation.LeoEcsExtensions.Systems
+{
+    /// <summary>
+    ///     System for removing entities with OneFrame components.
+    /// </summary>
+    /// <typeparam name="T">OneFrame component type.</typeparam>
+    internal sealed class RemoveOneFrameEntity<T> : IEcsRunSystem, IEcsInitSystem
+        where T : struct
+
+    {
+        private EcsFilter _filter;
+        private EcsWorld _world;
+
+        public void Init(EcsSystems systems)
+        {
+            _world = systems.GetWorld();
+            _filter = _world.Filter<T>().End();
+        }
+
+        public void Run(EcsSystems systems)
+        {
+            foreach (var i in _filter)
+            {
+                _world.DelEntity(i);
+            }
+        }
+    }
+}
